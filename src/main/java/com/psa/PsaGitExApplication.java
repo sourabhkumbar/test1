ba597878083e3ac759e6e63de873ae2d5622c953
sourabh1704 <sourabhkumbar99@gmail.com>
2025-04-04 09:42:29 +0530

    First Commit

---
+++ src/main/java/com/psa/PsaGitExApplication.java
@@ -0,0 +1,13 @@
+package com.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaGitExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaGitExApplication.class, args);
+	}
+
+}
